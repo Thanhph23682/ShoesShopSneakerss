d8b4dc5694b204d4336af750b2fd143cc7ddb4e7
Thanhph23682 <thanhndph23682@fpt.edu.vn>
2024-10-01 22:11:45 +0700

    ThanhDelete

--- FPolyShopSneakerss/FPolyShopSneakerss/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace FPolyShopSneakerss.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
