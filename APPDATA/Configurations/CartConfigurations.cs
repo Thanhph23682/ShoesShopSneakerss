4816767cec0906f52c35af728c7882dc9fd40928
Thanhph23682 <thanhndph23682@fpt.edu.vn>
2024-10-20 17:28:17 +0700

    Merged code Vinh

--- APPDATA/Configurations/CartConfigurations.cs
+++ APPDATA/Configurations/CartConfigurations.cs
@@ -1,6 +1,6 @@
 ﻿using APPDATA.Models;
-using Microsoft.EntityFrameworkCore.Metadata.Builders;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -9,6 +9,20 @@ using System.Threading.Tasks;
 
 namespace APPDATA.Configurations
 {
-   
-    
+    public class CartConfigurations : IEntityTypeConfiguration<Cart>
+    {
+        public void Configure(EntityTypeBuilder<Cart> builder)
+        {
+            builder.ToTable("Cart");
+            builder.HasKey(p => p.Id);
+            // Mối quan hệ với Customer
+            builder.HasOne<Customer>(p => p.Customers).WithMany(p => p.Carts).HasForeignKey(p => p.CustomerId).HasConstraintName("FK_Cart_Customer");
+            // Mối quan hệ với CartItems
+            builder.HasMany(p => p.CartItems) 
+                   .WithOne() 
+                   .HasForeignKey(p => p.CartId) 
+                  
+                   .HasConstraintName("FK_CartItems_Cart");
+        }
+    }
 }
