af108f5fb646cb6548f86f69275ea605d104d274
Bunky <huongpdph27798@fpt.edu.vn>
2024-10-24 16:23:51 +0700

    Code Dbcontext va service

---
+++ APPAPI/Services/CRUDapi.cs
@@ -0,0 +1,71 @@
+﻿using APPDATA.DB;
+using Microsoft.EntityFrameworkCore;
+
+namespace APPAPI.Services
+{
+    public class CRUDapi<T> where T : class
+    {
+        ShopDbContext _context;
+        DbSet<T> _dbSet;
+        public CRUDapi(ShopDbContext context, DbSet<T> dbset)
+        {
+           _context = context;
+            _dbSet = dbset;
+        }
+        public bool CreateItem(T item)
+        {
+            try
+            {
+                _dbSet.Add(item);
+                _context.SaveChanges();
+                return true;
+            }
+            catch (Exception)
+            {
+                return false;
+            }
+        }
+        public bool DeleteItem(T item)
+        {
+            try
+            {
+                _dbSet.Remove(item);
+                _context.SaveChanges();
+                return true;
+            }
+            catch (Exception)
+            {
+
+                return false;
+            }
+        }
+        public IEnumerable<T> GetAllItems()
+        {
+            try
+            {
+                return _dbSet.ToList();
+
+            }
+            catch (Exception)
+            {
+
+                return null;
+            }
+        }
+
+        public bool UpdateItem(T item)
+        {
+            try
+            {
+                _dbSet.Update(item);
+                _context.SaveChanges();
+                return true;
+            }
+            catch (Exception)
+            {
+
+                return false;
+            }
+        }
+    }
+}
