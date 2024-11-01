1e0c2f9a224a10bb7b042327e324cc5083a7deb8
thanhvinh1605 <vinhttph19362@fpt.edu.vn>
2024-11-01 22:37:35 +0700

    api + favicon bổ sung cho lợi làm giao diện

---
+++ APPAPI/Controllers/ContactController.cs
@@ -0,0 +1,74 @@
+﻿using APPAPI.Services;
+using APPDATA.DB;
+using APPDATA.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace APPAPI.Controllers
+{
+ [Route("api/[controller]")]
+ [ApiController]
+ public class ContactController : ControllerBase
+ {
+  ShopDbContext _context = new ShopDbContext();
+  private readonly CRUDapi<Contact> _crud;
+
+
+
+  public ContactController()
+  {
+   _crud = new CRUDapi<Contact>(_context, _context.Contacts);
+  }
+  [HttpGet]
+  public IEnumerable<Contact> GetAll()
+  {
+   return _crud.GetAllItems().ToList();
+  }
+  [Route("Create")]
+  [HttpPost]
+  public bool Create(Contact obj)
+  {
+   return _crud.CreateItem(obj);
+  }
+  [Route("Update")]
+  [HttpPut]
+  public bool Update(Contact obj)
+  {
+
+   Contact item = _crud.GetAllItems().FirstOrDefault(p => p.ContactID == obj.ContactID);
+   if (item != null)
+   {
+    item.ContactName = obj.ContactName;
+    item.ContactDescription = obj.ContactDescription;
+    item.ContactImageURL = obj.ContactImageURL;
+
+    return _crud.UpdateItem(item);
+   }
+   else
+   {
+    return false;
+   }
+
+
+  }
+  [Route("Delete")]
+  [HttpDelete]
+  public bool Delete(int? id)
+  {
+   Contact item = _crud.GetAllItems().FirstOrDefault(p => p.ContactID == id);
+
+   if (item != null)
+   {
+    // Nếu tìm thấy, thực hiện xóa
+    return _crud.DeleteItem(item);
+   }
+   else
+   {
+    // Nếu không tìm thấy, trả về false
+    return false;
+   }
+  }
+ }
+}
+
+
---
+++ APPAPI/Controllers/RoleController.cs
@@ -0,0 +1,78 @@
+﻿using APPAPI.Services;
+using APPDATA.DB;
+using APPDATA.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace APPAPI.Controllers
+{
+ [Route("api/[controller]")]
+ [ApiController]
+ public class RoleController : ControllerBase
+ {
+  ShopDbContext _context = new ShopDbContext();
+   private readonly CRUDapi<Role> _crud;
+  
+
+  
+
+ 
+  public RoleController()
+  {
+   _crud = new CRUDapi<Role>(_context, _context.Roles);
+  }
+
+
+  [HttpGet]
+  public IEnumerable<Role> GetAll() 
+  {
+   return _crud.GetAllItems().ToList();
+  }
+  [Route("Create")]
+  [HttpPost] 
+  public bool Create (Role obj)
+  {
+   return _crud.CreateItem(obj);
+  }
+  [Route("Update")]
+  [HttpPut]
+  public bool Update(Role obj)
+  {
+   Role item = _crud.GetAllItems().FirstOrDefault(p => p.ID == obj.ID);
+
+   if (item != null)
+   {
+    
+    item.RoleName = obj.RoleName;
+    item.Description = obj.Description;
+
+    
+    return _crud.UpdateItem(item);
+   }
+   else
+   {
+   
+    return false; 
+   }
+  }
+
+  [Route("Delete")]
+  [HttpDelete]
+  public bool Delete(int id)
+  {
+   Role item = _crud.GetAllItems().FirstOrDefault(p => p.ID == id);
+
+   if (item != null)
+   {
+    // Nếu tìm thấy, thực hiện xóa
+    return _crud.DeleteItem(item);
+   }
+   else
+   {
+    // Nếu không tìm thấy, trả về false
+    return false;
+   }
+  }
+
+ }
+}
