18d5faae5150a3714ae8d866040a7d2b91da13f0
thanhvinh1605 <vinhttph19362@fpt.edu.vn>
2024-12-30 23:25:59 +0700

    view model...

---
+++ APPMVC/ModelsViews/BillDetailViewModel.cs
@@ -0,0 +1,10 @@
+﻿namespace APPMVC.ModelsViews
+{
+    public class BillDetailViewModel
+    {
+        public string ProductName { get; set; }
+        public decimal Price { get; set; }
+        public int Quantity { get; set; }
+        public decimal SubTotal { get; set; }
+    }
+}
